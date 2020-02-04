f3f62dc3813e3c6486d04a13cd8722d591a20f67
nikolicsmilan <nikolicsmilan@gmail.com>
2020-02-04 18:58:25 +0100

    4 commit

--- kornyezet.sh
+++ kornyezet.sh
@@ -1,34 +1,53 @@
 #!bin/bash
 sudo apt update
-read -p "Press any key to continue... " -n1 -s
+read var_year
 sudo apt install nodejs
+read var_year
 sudo apt install npm
+read var_year
 nodejs -v
+read var_year
 npm install -g express-generator
+read var_year
 sudo apt update
+read var_year
 sudo apt-get remove docker docker-engine docker.io containerd runc
+read var_year
 sudo apt-get update
+read var_year
 sudo apt-get install
+read var_year
 sudo apt-get install \
  apt-transport-https \
  ca-certificates \
  curl \
  gnupg-agent \
  software-properties-common
+read var_year
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add –
+read var_year
 sudo apt-key fingerprint 0EBFCD88
+read var_year
 sudo add-apt-repository \
  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
  $(lsb_release -cs) \
  stable"
+read var_year
 sudo apt-get update
+read var_year
 sudo apt-get install docker-ce docker-ce-cli containerd.io
+read var_year
 sudo docker run hello-world
+read var_year
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/dockercompose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+read var_year
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose –version
+read var_year
 mkdir projects
+read var_year
 cd projects
+read var_year
 sudo fallocate -l 2G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
