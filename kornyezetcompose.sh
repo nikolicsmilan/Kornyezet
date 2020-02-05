1d71ab5405e36eed7660f3e33e08c7f2dd6a9f99
nikolicsmilan <nikolicsmilan@gmail.com>
2020-02-05 15:37:54 +0100

    25

--- kornyezetcompose.sh
+++ kornyezetcompose.sh
@@ -2,6 +2,7 @@ echo sudo curl "-L" "https://github.com/docker/compose/releases/download/1.25.0/
 echo sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 echo sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose- dali dali du -o /usr/local/bin/docker-compose
 echo sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose"-" dali dali du -o /usr/local/bin/docker-compose
+echo sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose"-"dali dali du -o /usr/local/bin/docker-compose
 echo sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/dockercompose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 echo sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/dockercompose"-"$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 read var_year
