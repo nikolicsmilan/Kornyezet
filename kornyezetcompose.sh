bc88e8174d447410e0ce597882a02b7df9145a9d
nikolicsmilan <nikolicsmilan@gmail.com>
2020-02-05 15:44:04 +0100

    megoldás_2

--- kornyezetcompose.sh
+++ kornyezetcompose.sh
@@ -1,4 +1,4 @@
 
 echo sudo curl -L 'https://github.com/docker/compose/releases/download/1.25.0/dockercompose-$(uname -s)-$(uname -m)' -o /usr/local/bin/docker-compose
 read var_year
-sudo curl "-L" "https://github.com/docker/compose/releases/download/1.25.0/dockercompose""-"""$(uname "-s")"-"$(uname "-m")"" "-o" /usr/local/bin/docker-compose
+sudo curl -L 'https://github.com/docker/compose/releases/download/1.25.0/dockercompose-$(uname -s)-$(uname -m)' -o /usr/local/bin/docker-compose
