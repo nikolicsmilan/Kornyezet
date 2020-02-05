88d87172e734ab12fd8c55c0e99b937e9b4247d6
nikolicsmilan <nikolicsmilan@gmail.com>
2020-02-05 15:33:31 +0100

    23

--- kornyezetcompose.sh
+++ kornyezetcompose.sh
@@ -1,4 +1,7 @@
 echo sudo curl "-L" "https://github.com/docker/compose/releases/download/1.25.0/dockercompose""-"""$(uname "-s")"-"$(uname "-m")"" "-o" /usr/local/bin/docker-compose
 echo sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+echo sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose- dali dali du -o /usr/local/bin/docker-compose
+echo sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose"-" dali dali du -o /usr/local/bin/docker-compose
+echo sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/dockercompose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 read var_year
 sudo curl "-L" "https://github.com/docker/compose/releases/download/1.25.0/dockercompose""-"""$(uname "-s")"-"$(uname "-m")"" "-o" /usr/local/bin/docker-compose
