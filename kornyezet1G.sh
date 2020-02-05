9d472d2d4c88c503be1ccd7ddbfb94bf6254346b
nikolicsmilan <nikolicsmilan@gmail.com>
2020-02-05 19:44:21 +0100

    megoldás_26

--- kornyezet1G.sh
+++ kornyezet1G.sh
@@ -5,10 +5,10 @@ read var_year
 sudo apt update
 echo  Ez lesz: sudo apt install nodejs 
 read var_year
-sudo apt install nodejs
-echo Ez lesz: sudo apt install npm 
+sudo apt install nodejs -y
+echo Ez lesz: sudo apt install npm -y
 read var_year
-sudo apt install npm
+sudo apt install npm 
 echo  Ez lesz: nodejs -v
 read var_year
 nodejs -v
@@ -21,7 +21,7 @@ sudo apt update
 echo  Ez lesz: sudo apt-get remove docker docker-engine docker.io containerd runc
 read var_year
 sudo apt-get remove docker docker-engine docker.io containerd runc
-echo Nyomj entert a folytatáshoz Ez lesz: sudo apt-get update
+echo  Ez lesz: sudo apt-get update
 read var_year
 sudo apt-get update
 echo  Ez lesz: sudo apt-get install
@@ -29,11 +29,11 @@ read var_year
 sudo apt-get install
 echo  Ez lesz: sudo apt-get install \ apt-transport-https \ ca-certificates \ curl \ gnupg-agent \ software-properties-common
 read var_year
-sudo apt-get install apt-transport-https 
-sudo apt-get install ca-certificates
-sudo apt-get install curl
-sudo ap-get install gnupg-agent 
-sudo apt-get install software-properties-common
+sudo apt-get install apt-transport-https -y
+sudo apt-get install ca-certificates -y
+sudo apt-get install curl -y
+sudo ap-get install gnupg-agent -y
+sudo apt-get install software-properties-common -y
 echo Ez lesz: curl "-fsSL" https://download.docker...
 read var_year
 curl "-fsSL" https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add "-"
@@ -51,7 +51,7 @@ read var_year
 sudo apt-get update
 echo Ez lesz: sudo apt-get install docker-ce docker-ce-cli containerd.io
 read var_year
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 echo  Ez lesz: sudo docker run hello-world
 read var_year
 sudo docker run hello-world
@@ -61,16 +61,16 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-
 echo  Ez lesz:sudo chmod +x /usr/local/bin/docker-compose 
 read var_year
 sudo chmod +x /usr/local/bin/docker-compose
-echo Ez lesz: docker-compose \–version
+echo Ez lesz: docker-compose "–"version
 read var_year
-docker-compose –version
+docker-compose version
 echo  Ez lesz:mkdir projects
 read var_year
 mkdir projects
 echo  Ez lesz: cd /projects
 read var_year
 cd /projects
-echo Ez lesz: sudo fallocate \-l 2G /swapfile
+echo Ez lesz: sudo fallocate "-"l 2G /swapfile
 read var_year
 sudo fallocate -l 2G /swapfile
 echo  Ez lesz: sudo chmod 600 /swapfile
@@ -82,7 +82,7 @@ sudo mkswap /swapfile
 echo  Ez lesz: sudo swapon /swapfile
 read var_year
 sudo swapon /swapfile
-echo  Ez lesz: sudo swapon \–show
+echo  Ez lesz: sudo swapon '–show'
 read var_year
 sudo swapon –show
 echo Vége a környzet beállításának!
