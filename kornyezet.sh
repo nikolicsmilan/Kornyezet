c235030add7368a2776ce63989e1b05e30540505
nikolicsmilan <nikolicsmilan@gmail.com>
2020-02-05 11:53:01 +0100

    15

--- kornyezet.sh
+++ kornyezet.sh
@@ -59,7 +59,7 @@ read var_year
 sudo docker run hello-world
 echo Nyomj entert a folytatáshoz Ez lesz: sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/dockercompose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 read var_year
-sudo curl "-L" "https://github.com/docker/compose/releases/download/1.25.0/dockercompose-$(uname -s)-$(uname -m)" "-o" /usr/local/bin/docker-compose
+sudo curl "-L" "https://github.com/docker/compose/releases/download/1.25.0/dockercompose-$(uname "-s")-$(uname "-m")" "-o" /usr/local/bin/docker-compose
 echo Nyomj entert a folytatáshoz Ez lesz:sudo chmod +x /usr/local/bin/docker-compose 
 read var_year
 sudo chmod +x /usr/local/bin/docker-compose
