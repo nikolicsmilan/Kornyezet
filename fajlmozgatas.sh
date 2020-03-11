8f1852807a24bec4cc96e58df7ebc58e2ec4520b
nikolicsmilan <nikolicsmilan@gmail.com>
2020-03-11 18:39:12 +0100

    'sdfg'

--- fajlmozgatas.sh
+++ fajlmozgatas.sh
@@ -1,2 +1,3 @@
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 9000
 cp /root/Kornyezet/000-default.conf /etc/apache2/sites-available/
 cp /root/Kornyezet/000-default-le-ssl.conf /etc/apache2/sites-available/
